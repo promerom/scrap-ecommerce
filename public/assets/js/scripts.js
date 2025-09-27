0df0704771749a71f402a6df6764c0d57a300269
Plinio Romero <romero.p.mfc@gmail.com>
2025-09-26 21:16:11 -0500

    Agrega archivo de scripts frontend (scripts.js)

---
+++ public/assets/js/scripts.js
@@ -0,0 +1,6 @@
+// Lógica simple para favoritos (puedes expandir según la API)
+$(function() {
+  $('#favorite-btn').on('click', function() {
+    alert('Producto agregado a favoritos!');
+  });
+});
